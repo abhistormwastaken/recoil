d4f491893ea1d571db2e1bb96bc1a3b992730fcc
abhistormwastaken <abhistorm409@gmail.com>
2024-01-31 15:42:59 +0530

    Updated Recoil selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
-import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
-import { countAtom } from '../store/atoms/count'
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { countAtom, evenSelector } from '../store/atoms/count'
 
 function App() {
   
@@ -17,6 +17,20 @@ function CountRenderer() {
   const count = useRecoilValue(countAtom)
   return <div>
     {count}
+    <EvenRenderer></EvenRenderer>
+  </div>
+}
+
+function EvenRenderer() {
+  // const count = useRecoilValue(countAtom)
+  // return <div>
+  //   {count % 2 === 0 ? "Even" : "Odd"}
+  // </div>
+
+  // but this is not the best as we are running the isEven logic everytime this component will re-render (due to another state in this component or another state variable change in the parent component), this would not be a problem if the logic is simple but if it is complex then it will be a problem (like useMemo to prevent expensive logic to run, we use selectors in recoil)
+  const isEven = useRecoilValue(evenSelector)
+  return <div>
+    {isEven ? "It is Even" : null}
   </div>
 }
 
@@ -31,10 +45,13 @@ function Count() {
 }
 
 function ButtonRenderer() {
-  const [count, setCount] = useRecoilState(countAtom)
+  console.log("Button does not re-render as the state is managed by recoil")
+  // const setCount = useRecoilState(countAtom)[1]  OR
+  const setCount = useSetRecoilState(countAtom)
+  
   return <div>
-    <button onClick={() => {setCount(count + 1)}}>Increment</button>
-    <button onClick={() => {setCount(count - 1)}}>Decrement</button>
+    <button onClick={() => {setCount(count => count + 1)}}>Increment</button>
+    <button onClick={() => {setCount(count => count - 1)}}>Decrement</button>
   </div>
 }
 
--- store/atoms/count.jsx
+++ store/atoms/count.jsx
@@ -1,7 +1,15 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 // atom for count state variable
 export const countAtom = atom({
     key: 'countAtom',
     default: 0
+})
+
+export const evenSelector = selector({
+    key: "evenSelector",
+    get: ({get}) => {
+        const count = get(countAtom)
+        return count % 2 == 0
+    }
 })
